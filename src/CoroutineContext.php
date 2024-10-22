f775226f80fe7faebb4ec5caa9778aafa5be8289
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-22 20:38:08 +0300

    + CoroutineContext

--- src/CoroutineContext.php
+++ src/CoroutineContext.php
@@ -4,60 +4,115 @@ declare(strict_types=1);
 namespace IfCastle\Swoole;
 
 use IfCastle\Async\CoroutineContextInterface;
+use Swoole\Coroutine;
 
-class CoroutineContext implements CoroutineContextInterface
+class CoroutineContext              implements CoroutineContextInterface
 {
     #[\Override]
     public function isCoroutine(): bool
     {
-        // TODO: Implement isCoroutine() method.
+        return Coroutine::getCid() !== -1;
     }
     
     #[\Override]
     public function getCoroutineId(): string|int
     {
-        // TODO: Implement getCoroutineId() method.
+        return Coroutine::getCid();
     }
     
     #[\Override]
     public function getCoroutineParentId(): string|int
     {
-        // TODO: Implement getCoroutineParentId() method.
+        return Coroutine::getPcid();
     }
     
     #[\Override]
     public function has(string $key): bool
     {
-        // TODO: Implement has() method.
+        // Get the current coroutine ID
+        $cid                        = Coroutine::getCid();
+        
+        do
+        {
+            /*
+             * Get the context object using the current coroutine
+             * ID and check if our key exists, looping through the
+             * coroutine tree if we are deep inside sub coroutines.
+             */
+            if(Coroutine::getContext($cid)?->offsetExists($key)) {
+                return true;
+            }
+            
+            // We may be inside a child coroutine, let's check the parent ID for a context
+            $cid                    = Coroutine::getPcid($cid);
+            
+        } while ($cid !== -1 && $cid !== false);
+        
+        return false;
     }
     
     #[\Override]
     public function get(string $key): mixed
     {
-        // TODO: Implement get() method.
+        // Get the current coroutine ID
+        $cid                        = Coroutine::getCid();
+        
+        do
+        {
+            /*
+             * Get the context object using the current coroutine
+             * ID and check if our key exists, looping through the
+             * coroutine tree if we are deep inside sub coroutines.
+             */
+            if(Coroutine::getContext($cid)?->offsetExists($key)) {
+                return Coroutine::getContext($cid)[$key];
+            }
+            
+            // We may be inside a child coroutine, let's check the parent ID for a context
+            $cid                    = Coroutine::getPcid($cid);
+            
+        } while ($cid !== -1 && $cid !== false);
+        
+        return null;
     }
     
     #[\Override]
     public function getLocal(string $key): mixed
     {
-        // TODO: Implement getLocal() method.
+        $context                    = Coroutine::getContext();
+        
+        if($context === null) {
+            return null;
+        }
+        
+        return $context[$key] ?? null;
     }
     
     #[\Override]
     public function hasLocal(string $key): bool
     {
-        // TODO: Implement hasLocal() method.
+        $context                    = Coroutine::getContext();
+        
+        if($context === null) {
+            return false;
+        }
+        
+        return isset($context[$key]);
     }
     
     #[\Override]
     public function set(string $key, mixed $value): static
     {
-        // TODO: Implement set() method.
+        Coroutine::getContext()[$key] = $value;
+        
+        return $this;
     }
     
     #[\Override]
     public function defer(callable $callback): static
     {
-        // TODO: Implement defer() method.
+        Coroutine::defer($callback);
+        
+        return $this;
     }
 }
